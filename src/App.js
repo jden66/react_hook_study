7703ea94dd842fa4d06f2aa4f1d7ca66b62f03e4
jden66 <102334371+jden66@users.noreply.github.com>
2022-04-09 19:25:24 +0900

    add useTabs hook practice hook - useTabs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import "./styles.css";
 
+/**
+ * input의 상태를 관리하는 hook를 만든다.
+ * @param {number} initialValue
+ * @param {function} validator
+ */
 function useInput(initialValue, validator) {
   const [value, setValue] = useState(initialValue);
   const onChange = (e) => {
@@ -17,15 +22,55 @@ function useInput(initialValue, validator) {
   return { value, onChange };
 }
 
+const sections = [
+  {
+    name: "section 1",
+    context: "This is section 1"
+  },
+  {
+    name: "section 2",
+    context: "This is section 2"
+  }
+];
+/**
+ * tab의 상태를 반환하는 hook을 만든다.
+ * @param {number} initialIndex
+ * @param {Array} allTabs
+ */
+function useTabs(initialIndex, allTabs) {
+  const [currentIndex, setCurrentIndex] = useState(initialIndex);
+  if (!Array.isArray(allTabs)) {
+    return;
+  }
+
+  return {
+    currentItem: allTabs[currentIndex],
+    setCurrentItem: setCurrentIndex
+  };
+}
+
 export default function App() {
   // const maxLen = (value) => value.length <= 10;
   const limitWord = (value) => !value.includes("@");
   const name = useInput("Mr.", limitWord);
+
+  const { currentItem, setCurrentItem } = useTabs(0, sections);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <input {...name} />
+      <div>
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <input {...name} />
+      </div>
+      <br />
+      <div>
+        {sections.map((section, index) => (
+          <button key={index} onClick={() => setCurrentItem(index)}>
+            {section.name}
+          </button>
+        ))}
+        <div>{currentItem.context}</div>
+      </div>
     </div>
   );
 }
